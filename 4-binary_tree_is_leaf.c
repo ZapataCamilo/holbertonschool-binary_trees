5f178c9214aa1bb51cf8db517768b10b356a38fb
YeosCRN <jeoscrn20@outlook.com>
2023-01-17 14:27:36 -0500

    4.0

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if node is leaf
+ * @node: pointer target node
+ * Return: 1 if leaf, 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+	return (1);
+}
