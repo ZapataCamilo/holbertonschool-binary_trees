1cbff949fffb373e5d2689f9f7e871fa14ad794a
YeosCRN <jeoscrn20@outlook.com>
2023-01-17 14:17:49 -0500

    3.0

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_delete - deletes a binary tree and its children
+ * @tree: pointer to head to the tree
+ * Return: if tree = NULL
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-        return;
+	if (!tree)
+		return;
 
-    while (tree->left != NULL)
-        free(tree->left);
-    while (tree->right != NULL)
-        free(tree->right);
-    free(tree->parent);
-    free(tree);
+	free(tree);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
 }
