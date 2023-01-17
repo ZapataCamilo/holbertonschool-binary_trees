a0cf383c2c874891b4f345949967add355ee865e
YeosCRN <jeoscrn20@outlook.com>
2023-01-17 13:39:12 -0500

    2.1

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,21 +8,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *right_child;
+	binary_tree_t *right_child;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    right_child = binary_tree_node(parent, value);
-    if (right_child == NULL)
-        return (NULL);
+	right_child = binary_tree_node(parent, value);
+	if (right_child == NULL)
+		return (NULL);
 
-    if (parent->left != NULL)
-    {
-        right_child->left = parent->left;
-        parent->left->parent = right_child;
-    }
-    parent->left = right_child;
+	if (parent->right != NULL)
+	{
+		right_child->right = parent->right;
+		parent->right->parent = right_child;
+	}
+	parent->right = right_child;
 
-    return (right_child);
+	return (right_child);
 }
