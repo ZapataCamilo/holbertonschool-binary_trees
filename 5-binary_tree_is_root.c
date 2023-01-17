e700b543dbef1d599784f76b1be221cdd4370fd5
YeosCRN <jeoscrn20@outlook.com>
2023-01-17 15:46:18 -0500

    .0

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node == NULL || node->left == NULL || node->right == NULL)
+		return (0);
+	return (1);
+}
