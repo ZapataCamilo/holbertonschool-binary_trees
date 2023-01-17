0279928500aeb00a52a66622d0ef41651900e210
YeosCRN <jeoscrn20@outlook.com>
2023-01-17 13:25:35 -0500

    1.2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,22 +2,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *left_child;
+	binary_tree_t *left_child;
 
-    left_child = malloc(sizeof(binary_tree_t));
+	left_child = malloc(sizeof(binary_tree_t));
 
-    if (left_child == NULL)
-    {
-        return (NULL);
-    }
-    left_child->n = value;
-    left_child->parent = parent;
-    if (parent->left != NULL)
-    {
-        left_child->left = parent->left;
-        parent->left->parent = left_child;
-    }
-    parent->left = left_child;
+	if (left_child == NULL)
+	{
+		return (NULL);
+	}
+	left_child = binary_tree_node(parent, value);
+	if (left_child == NULL)
+		return (NULL);
 
-    return (left_child);
+	if (parent->left != NULL)
+	{
+		left_child->left = parent->left;
+		parent->left->parent = left_child;
+	}
+	parent->left = left_child;
+
+	return (left_child);
 }
