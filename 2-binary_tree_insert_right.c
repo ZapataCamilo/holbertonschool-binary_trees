bdd3b60a7bc30478fc1cf5019ab8ff0e4a8a9ab9
YeosCRN <jeoscrn20@outlook.com>
2023-01-17 13:40:41 -0500

    2.2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -  creates a right child for parent
+ * binary_tree_insert_right -  creates a right child for parent
  * @parent:  is a pointer to the parent node of the node to create
  * @value:  is the value to put in the new node
  * Return:  a pointer to the new node
