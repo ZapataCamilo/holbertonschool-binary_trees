ae5016c4d9f53d96753ef4951d002ad5bce18ea4
YeosCRN <jeoscrn20@outlook.com>
2023-01-19 13:53:04 -0500

    13.0

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_nodes - counts the number of nodes with at least 1 child
  * @tree: is a pointer to the root node of the tree to measure the size
- * Return: Number of leaves, 0 if tree is NULL
+ * Return: Number of nodes, 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
